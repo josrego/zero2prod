22a361669d31d642fc137c174309784fa44029b2
Jose <jose@Joses-MBP.lan>
2024-06-17 08:25:22 +0100

    Project skeleton

---
+++ target/debug/deps/zero2prod-794010f837781678.d
@@ -0,0 +1,5 @@
+/Users/jose/projects/zero2prod/target/debug/deps/libzero2prod-794010f837781678.rmeta: src/main.rs
+
+/Users/jose/projects/zero2prod/target/debug/deps/zero2prod-794010f837781678.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/zero2prod-de063bfce875d9a5.d
@@ -0,0 +1,5 @@
+/Users/jose/projects/zero2prod/target/debug/deps/libzero2prod-de063bfce875d9a5.rmeta: src/main.rs
+
+/Users/jose/projects/zero2prod/target/debug/deps/zero2prod-de063bfce875d9a5.d: src/main.rs
+
+src/main.rs:
